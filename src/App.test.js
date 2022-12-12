6afd072ac9df4ecb29ba228d1f1feb080fade194
besh612 <sung.hun5016@gmail.com>
2022-12-12 23:27:56 +0900

    test: add button fire event

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("Button has correct initial color with text", () => {
@@ -6,4 +6,13 @@ test("Button has correct initial color with text", () => {
   // find an element with a role of button and text of "Change to blue"
   const colorButton = screen.getByRole("button", { name: "Change to blue" });
   expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+});
+
+test("Button color and text is changed when button clicked", () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+  expect(colorButton).toHaveTextContent("Change to red");
 });
