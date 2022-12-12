d8fefda09bb624eab7f25ed25127828c9d466860
besh612 <sung.hun5016@gmail.com>
2022-12-12 21:04:16 +0900

    feat: add red button with 'Change to blue ' text

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <button style={{ backgroundColor: "red" }}>Change to blue</button>
+    </div>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -5,5 +5,5 @@ test("Button has correct initial color with text", () => {
   render(<App />);
   // find an element with a role of button and text of "Change to blue"
   const colorButton = screen.getByRole("button", { name: "Change to blue" });
-  expect(colorButton).toHaveStyle({ backgroundColor: "red"`` });
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
 });
