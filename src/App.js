fb643c5c9fe90e910a0c36f27f5ca7b9fb91292e
besh612 <sung.hun5016@gmail.com>
2022-12-12 23:30:05 +0900

    feat: add onclick color changer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("red");
+  const colorChanger = color === "red" ? "blue" : "red";
   return (
     <div className="App">
-      <button style={{ backgroundColor: "red" }}>Change to blue</button>
+      <button
+        style={{ backgroundColor: color }}
+        onClick={() => setColor(colorChanger)}
+      >
+        {`Change to ${colorChanger}`}
+      </button>
     </div>
   );
 }
